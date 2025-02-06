1a5c723fc5ad994b5252b3dd284b136544ad6eee
namki <netzzang12@gmail.com>
2025-02-06 09:16:30 +0900

    3자리숫자입력한건지 확인 - isThreeNum() 추가

--- src/main/java/numBaseBall/Baseball.java
+++ src/main/java/numBaseBall/Baseball.java
@@ -6,6 +6,7 @@ public class Baseball {
     private List<String> solutionBalls = new ArrayList<>();
     private Scanner sc = new Scanner(System.in);
 
+
     private void addBalls() {
         List<String> candiBalls = new ArrayList<>();
         for (int i = 1; i < 10; i++) {
@@ -15,14 +16,26 @@ public class Baseball {
         solutionBalls.addAll(List.of(candiBalls.get(0), candiBalls.get(1), candiBalls.get(2)));
     }
 
+
+    private boolean isThreeNum(String str) {
+        return str.matches("^[1-9]{3}$");
+    }
+
+
     private void play() {
         addBalls();
         System.out.println(solutionBalls);
         while (true) {
-            System.out.println("숫자 3개를 입력하세요");
+            System.out.println("1-9사이의 숫자 3개를 입력하세요");
             String inputNumbers = sc.nextLine();
-            List<String> inputBalls = new ArrayList<>();
-            inputBalls.addAll(List.of(inputNumbers.split("")));
+
+            if (!isThreeNum(inputNumbers)) {
+                System.out.print("잘못된 형식입니다. ");
+                continue;
+            }
+
+            List<String> inputBalls;
+            inputBalls = new ArrayList<>(List.of(inputNumbers.split("")));
             int strikeCount = 0;
             int ballCount = 0;
 
@@ -52,4 +65,5 @@ public class Baseball {
         Baseball game = new Baseball();
         game.play();
     }
+
 }
